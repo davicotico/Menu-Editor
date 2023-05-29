4612c56c6f8cdf658168e887ea67d0a966dcff88
davicotico <david.ticona.saravia@gmail.com>
2023-05-28 21:03:37 -0400

    v1.0.0 para NPM

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,5 @@
+/**
+ * @author  David Tomas Ticona Saravia <davicotico@yandex.com>
+ * @license MIT
+ */
 export { default as MenuEditor } from './MenuEditor';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+/**
+ * @author  David Tomas Ticona Saravia <davicotico@yandex.com>
+ * @license MIT
+ */
 import './scss/styles.scss';
 export { default as MenuEditor } from './MenuEditor';
